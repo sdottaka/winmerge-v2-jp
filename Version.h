a6b6affefd70e519c554af4003de4125d90a0bd9
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2026-02-26 21:29:50 +0900

    Bump revision to 2.16.54+-jp-3

--- Version.h
+++ Version.h
@@ -1,6 +1,6 @@
-#define FILEVER        2,16,54,2
-#define PRODUCTVER     2,16,54,2
-#define STRFILEVER     "2.16.54.2"
-#define STRPRODUCTVER  "2.16.54.2"
-#define STRPRIVATEBUILD "jp-2"
+#define FILEVER        2,16,54,3
+#define PRODUCTVER     2,16,54,3
+#define STRFILEVER     "2.16.54.3"
+#define STRPRODUCTVER  "2.16.54.3"
+#define STRPRIVATEBUILD "jp-3"
 #define STRYEARMONTH   "2026.03"
