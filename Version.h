9d38008cfb0440c0f931b7966e23e297b4cfe449
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2026-07-01 18:41:17 +0900

    Bump revision to WinMerge 2.16.56+-jp-5

--- Version.h
+++ Version.h
@@ -1,6 +1,6 @@
-#define FILEVER        2,16,56,4
-#define PRODUCTVER     2,16,56,4
-#define STRFILEVER     "2.16.56.4"
-#define STRPRODUCTVER  "2.16.56.4"
-#define STRPRIVATEBUILD "jp-4"
+#define FILEVER        2,16,56,5
+#define PRODUCTVER     2,16,56,5
+#define STRFILEVER     "2.16.56.5"
+#define STRPRODUCTVER  "2.16.56.5"
+#define STRPRIVATEBUILD "jp-5"
 #define STRYEARMONTH   "2026.06"
